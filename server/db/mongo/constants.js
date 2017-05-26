b2df00ee17647069d096a77787160e945ff70fde
Muhammad Abbas <mohammed.petiwala52@gmail.com>
2017-05-26 12:07:05 -0700

    wip: added mongolab addon in heroku and added uri in db constants

--- server/db/mongo/constants.js
+++ server/db/mongo/constants.js
@@ -1,5 +1,5 @@
 // export const db = process.env.MONGOHQ_URL || process.env.MONGODB_URI || 'mongodb://localhost/ReactWebpackNode';
-export const db = process.env.MONGOHQ_URL || process.env.MONGODB_URI || 'mongodb://heroku_1p903gx7:rtipp81bt3usttno0e89e1v1ng@ds027709.mlab';
+export const db = process.env.MONGOHQ_URL || process.env.MONGODB_URI || 'mongodb://heroku_47kh9fld:hrbdtn3coro52vbrcho4i5ti3f';
 
 export default {
   db
