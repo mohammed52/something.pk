a233f392d669039bc3a83d1b99f7252a5e715d01
Muhammad Abbas <mohammed.petiwala52@gmail.com>
2017-05-26 11:44:31 -0700

    changed mongodb uri

--- server/db/mongo/constants.js
+++ server/db/mongo/constants.js
@@ -1,5 +1,5 @@
-export const db = process.env.MONGOHQ_URL || process.env.MONGODB_URI || 'mongodb://localhost/ReactWebpackNode';
-// export const db = process.env.MONGOHQ_URL || process.env.MONGODB_URI || 'mongodb://heroku_1p903gx7:rtipp81bt3usttno0e89e1v1ng@ds027709.mlab';
+// export const db = process.env.MONGOHQ_URL || process.env.MONGODB_URI || 'mongodb://localhost/ReactWebpackNode';
+export const db = process.env.MONGOHQ_URL || process.env.MONGODB_URI || 'mongodb://heroku_1p903gx7:rtipp81bt3usttno0e89e1v1ng@ds027709.mlab';
 
 export default {
   db
