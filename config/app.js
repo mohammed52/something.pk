f6ed5f541ff07cdcac73da91e11a0c7fa3df3e87
Muhammad Abbas <mohammed.petiwala52@gmail.com>
2017-08-02 09:26:06 -0700

    changed to https

--- config/app.js
+++ config/app.js
@@ -4,7 +4,7 @@ export const isProduction = ENV === 'production';
 export const isDebug = ENV === 'development';
 export const isClient = typeof window !== 'undefined';
 
-export const apiEndpoint = isDebug ? 'http://localhost:5000' : 'http://thawing-lake-20958.herokuapp.com';
+export const apiEndpoint = isDebug ? 'http://localhost:5000' : 'https://thawing-lake-20958.herokuapp.com';
 // export const apiEndpoint = isDebug ? 'http://localhost:5000' : 'http://localhost:5000';
 // Replace with 'UA-########-#' or similar to enable tracking
 export const trackingID = null;
