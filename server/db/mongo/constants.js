62676daa2079f0f2a4af70d4a921ef81a44e3714
Muhammad Abbas <mohammed.petiwala52@gmail.com>
2017-08-01 21:37:58 -0700

    edited config settings

--- server/db/mongo/constants.js
+++ server/db/mongo/constants.js
@@ -1,5 +1,5 @@
 // export const db = process.env.MONGOHQ_URL || process.env.MONGODB_URI || 'mongodb://localhost/Somethingpk';
-export const db = process.env.MONGOHQ_URL || process.env.MONGODB_URI || 'mongodb://heroku_47kh9fld:hrbdtn3coro52vbrcho4i5ti3f';
+export const db = process.env.MONGOHQ_URL || process.env.MONGODB_URI || 'mongodb://heroku_47kh9fld:hrbdtn3coro52vbrcho4i5ti3f@ds155411.mlab.com:55411/heroku_47kh9fld';
 
 export default {
   db
